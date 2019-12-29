8803f824aaedf87e9bceacdf96861579e74e86bf
Olesya Miller <olesyam.miller@gmail.com>
2019-12-29 03:15:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :status, :amount 
+  attr_writer :status 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+  
 end
+
