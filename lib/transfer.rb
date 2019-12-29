73a4485c2f2f9cdf870f37190d2ae27eca8983b8
Olesya Miller <olesyam.miller@gmail.com>
2019-12-29 04:43:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,6 @@ class Transfer
   
   def execute_transaction
     if  valid?
-      binding.pry 
       self.sender.balance -= self.amount 
       self.receiver.balance += self.amount
       self.status = "complete"
