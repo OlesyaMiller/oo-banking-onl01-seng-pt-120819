9a14c4b23711be8c12557c4228503b539e0b95d3
Olesya Miller <olesyam.miller@gmail.com>
2019-12-29 04:33:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       self.receiver.balance += self.amount
       self.status = "complete"
     else 
-      "Transaction rejected. Please check your account balance."
+      self.status = "rejected"
     end
   end
 end
